610e78d2f090bdfe35c584564c66b2b76f5c7bc3
Gerhard Baliet <gerhardbaliet@yahoo.de>
2023-06-08 23:20:34 +0200

    Added new router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
   { path: '', component : StartscreenComponent },
+  { path: 'game', component : GameComponent },
 ];
 
 @NgModule({
--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
   templateUrl: './startscreen.component.html',
   styleUrls: ['./startscreen.component.scss']
 })
-export class StartscreenComponent {
+export class StartscreenComponent implements OnInit {
+
+  constructor(private router: Router) { }
+  ngOnInit() {
+  }
+
+  startGame() {
+    console.log("start game");
+    this.router.navigate(['/game']);
+  }
+
 
 }
