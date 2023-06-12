02e89b13f2a7dec656cdf85b8ac402445ed14862
Gerhard Baliet <gerhardbaliet@yahoo.de>
2023-06-12 10:33:52 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,24 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import {
+  CollectionReference,
+  DocumentData,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from '@firebase/firestore';
+import { Firestore, collectionData, docData } from '@angular/fire/firestore';
 
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
 @NgModule({
@@ -42,6 +59,10 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatFormFieldModule,
     MatCardModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
 
   ],
   providers: [],
