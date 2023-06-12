dd48ec4f6bdaa77b66bd02f990af0345c43b0649
Gerhard Baliet <gerhardbaliet@yahoo.de>
2023-06-12 10:34:17 +0200

    Added firestore

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-dbe91',
+    appId: '1:786944372853:web:0c767427a63b984f5ec83c',
+    storageBucket: 'ring-of-fire-dbe91.appspot.com',
+    apiKey: 'AIzaSyBMNeJaRZ9QwdHYGtWkzrAhOm85R5G-8hM',
+    authDomain: 'ring-of-fire-dbe91.firebaseapp.com',
+    messagingSenderId: '786944372853',
+    measurementId: 'G-G5Z3NJBQM5',
+  },
+};
