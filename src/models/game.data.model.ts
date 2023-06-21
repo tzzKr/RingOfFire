01276340c24de2386fbb22ae3e3c4d503ce01043
Gerhard Baliet <gerhardbaliet@yahoo.de>
2023-06-21 19:20:09 +0200

    fix clean

--- src/models/game.data.model.ts
+++ src/models/game.data.model.ts
@@ -6,21 +6,3 @@ export interface GameJson {
     pickCardAnimation: boolean;
     currentCard: any;
 }
- 
-// export interface GameData {
-//     players: string[];
-//     stack: string[];
-//     playedCards: string[];
-//     currentPlayer: number;
-//     pickCardAnimation: boolean;
-//     currentCard: any;
-//     toJson: () => {
-//       players: string[];
-//       stack: string[];
-//       playedCards: string[];
-//       currentPlayer: number;
-//       pickCardAnimation: boolean;
-//       currentCard: any;
-//     };
-//   }
-  
